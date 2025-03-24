18bbdeeb4deabf260cd54fa16e583a4aa164e14b
DevWils26 <devwils@siue.edu>
2025-03-24 09:33:40 -0500

    Part 1 and Part 2

---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,179 @@
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S{get; private set;} // Singleton property
+
+    [Header("Inscribed")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
+    public Weapon[] weapons;
+
+    [Header("Dynamic")] [Range(0, 4)] [SerializeField]
+    private float _shieldLevel = 1; // Remember the underscore
+    // public float shieldLevel = 1; // and remove this old line
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
+    // Declare a new delegate type WeaponFireDelegate
+    public delegate void WeaponFireDelegate();
+    // Create a WeaponFireDelegate event named fireEvent
+    public event WeaponFireDelegate fireEvent;
+
+    void Awake()
+    {
+        if (S == null){
+            S = this; // Set the Singleton only if it's null
+        }
+        else{
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+        // fireEvent += TempFire;
+
+        // Reset the weapons to start _Hero wit 1 blaster
+        ClearWeapons();
+        weapons[0].SetType(eWeaponType.blaster);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pull in information from the Input class
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
+
+        // // Allow the ship to fire
+        // if (Input.GetKeyDown(KeyCode.Space)){
+        //     TempFire();
+        // }
+
+        // Use the fireEvent to fire Weapons when the Spacebar is pressed.
+        if (Input.GetAxis("Jump") == 1 && fireEvent != null){
+            fireEvent();
+        }
+    }
+
+    // void TempFire(){
+    //     GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+    //     projGO.transform.position = transform.position;
+    //     Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+    //     // rigidB.linearVelocity = Vector3.up * projectileSpeed;
+
+    //     ProjectileHero proj = projGO.GetComponent<ProjectileHero>();
+    //     proj.type = eWeaponType.blaster;
+    //     float tSpeed = Main.GET_WEAPON_DEFINITION(proj.type).velocity;
+    //     rigidB.linearVelocity = Vector3.up * tSpeed;
+    // }
+
+    void OnTriggerEnter(Collider other){
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go  = rootT.gameObject;
+        // Debug.Log("Shield trigger hit by: " +go.gameObject.name);
+
+        // Make sure it's not the same triggering go as last time
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        PowerUp pUp = go.GetComponent<PowerUp>();
+        if (enemy != null){ // If the shield was triggered by an enemy
+            shieldLevel--;
+            Destroy(go); // ... and Destroy the enemy
+        }
+        else if (pUp != null){  // If the shield hit a PowerUp
+            AbsorbPowerUp(pUp);  // ... absorb the PowerUp
+        }
+        else{
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " + go.name);
+        }
+    }
+
+    public void AbsorbPowerUp(PowerUp pUp) {
+        Debug.Log("Absorbed PowerUp: " + pUp.type);
+        switch (pUp.type) {
+            case eWeaponType.shield:
+                shieldLevel++;
+                break;
+
+            default:
+                // Check if any weapon slot has the same type as the new power-up
+                bool weaponAssigned = false;
+
+                for (int i = 0; i < weapons.Length; i++) {
+                    if (weapons[i].type == pUp.type) {
+                        // If the weapon already exists in the slot, do nothing
+                        Debug.Log("Weapon already equipped in slot " + i);
+                        weaponAssigned = true;
+                        break;
+                    }
+                }
+
+                if (!weaponAssigned) {
+                    // If no matching weapon, assign the new power-up to the first empty slot
+                    Weapon weap = GetEmptyWeaponSlot();
+                    if (weap != null) {
+                        weap.SetType(pUp.type);
+                        Debug.Log("Assigned " + pUp.type + " to empty slot.");
+                    } 
+                    else {
+                        // If no empty slot is available, replace the first weapon with the new one
+                        ClearWeapons();  // This will reset all weapons
+                        weapons[0].SetType(pUp.type);
+                        Debug.Log("Assigned " + pUp.type + " to weapons[0] after clearing.");
+                    }
+                }
+                break;
+        }
+
+        // Mark the power-up as absorbed
+        pUp.AbsorbedBy(this.gameObject);
+    }
+
+
+
+    public float shieldLevel{
+        get {return (_shieldLevel);}
+        private set{
+            _shieldLevel = Mathf.Min(value, 4);
+            // If the shield is going to be set to less than zero...
+            if (value < 0){
+                Destroy(this.gameObject); // Destroy the Hero
+                Main.HERO_DIED();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Finds the first empty Weapon slot (i.e., type=none) and returns it.
+    /// <\summary>
+    /// <returns>The first empty Weapon slot or null if none are empty</return>
+    Weapon GetEmptyWeaponSlot(){
+        for (int i = 0; i < weapons.Length; i++){
+            if (weapons[i].type == eWeaponType.none){
+                return(weapons[i]);
+            }
+        }
+        return(null);
+    }
+
+    /// <summary>
+    /// Sets the type of all Weapon slots to none
+    /// </summary>
+    void ClearWeapons(){
+        foreach (Weapon w in weapons){
+            w.SetType(eWeaponType.none);
+        }
+    }
+}
---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,135 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Enables the loading & reloading of scenes
+
+public class Main : MonoBehaviour
+{
+    static private Main S; // A private singleton for Main
+    static private Dictionary<eWeaponType, WeaponDefinition> WEAP_DICT;
+
+    [Header("Inscribed")]
+    public bool spawnEnemies = true;
+    public GameObject[] prefabEnemies; // Array of Enemy prefabs
+    public float enemySpawnPerSecond = 0.5f; // # Enemies spawned/second
+    public float enemyInsetDefault = 1.5f; // Inset from the sides
+    public float gameRestartDelay = 2;
+    public GameObject prefabPowerUp;
+    public WeaponDefinition[] weaponDefinitions;
+    public eWeaponType[] powerUpFrequency = new eWeaponType[]{
+        eWeaponType.blaster, eWeaponType.blaster, eWeaponType.spread, eWeaponType.shield
+    };
+    private BoundsCheck bndCheck;
+    private AudioSource audioSource;
+
+    void Start(){
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
+
+        // Ensure the audio is playing
+        if (audioSource && !audioSource.isPlaying){
+            audioSource.Play();
+        }
+    }
+    
+    void Awake(){
+        S = this;
+        // Set bndCheck to reference the BoundsCheck component on this
+        // GameObject
+        bndCheck = GetComponent<BoundsCheck>();
+
+        // Invoke SpawnEnemy() once (in 2 seconds, based on default values)
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+
+        // A generic Dictionary with eWeaponType as the key
+        WEAP_DICT = new Dictionary<eWeaponType, WeaponDefinition>();
+        foreach (WeaponDefinition def in weaponDefinitions){
+            WEAP_DICT[def.type] = def;
+        }
+    }
+
+    public void SpawnEnemy(){
+        // If spawnEnemies is false, skip to the next invoke of SpawnEnemy()
+        if (!spawnEnemies){
+            Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+            return;
+        }
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        // Position the Enemy above the screen with a random x position
+        float enemyInset = enemyInsetDefault;
+        if (go.GetComponent<BoundsCheck>() != null){
+            enemyInset = Mathf.Abs(go.AddComponent<BoundsCheck>().radius);
+        }
+
+        // Set the initial position for the spawned Enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyInset;
+        float xMax = bndCheck.camHeight - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+
+        // Invoke SpawnEnemy() again
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+
+    void DelayedRestart(){
+        // Invoke the Restart() method in gameRestartDelay seconds
+        Invoke(nameof(Restart), gameRestartDelay);
+    }
+
+    void Restart(){
+        // Reload __Scene_0 to restart the game
+        // "__Scene_0" below starts with 2 underscores and ends with a zero
+        SceneManager.LoadScene("__Scene_0");
+    }
+
+    static public void HERO_DIED(){
+        S.DelayedRestart();
+    }
+
+    /// <summary>
+    /// Static function that gets a WeaponDefinition from the WEAP_DICT static
+    /// protected field of the Main Class
+    /// </summary>
+    /// <returns>The WeaponDefiniton, or if there is no WeaponDefinition with
+    /// the eWeaponType passed in, returns a new WeaponDefinition with a
+    /// eWeaponType of WeaponType.none.</return>
+    /// <param name="wt">The eWeaponType of the desired WeaponDefiniton</param>
+    static public WeaponDefinition GET_WEAPON_DEFINITION(eWeaponType wt){
+        if (WEAP_DICT.ContainsKey(wt)){
+            return(WEAP_DICT[wt]);
+        }
+        // If no entry of the correct type exists in WEAP_DICT, return a new
+        // WeaponDefinition with a type of eWeaponType.none (the default value)
+        return(new WeaponDefinition());
+    }
+
+    /// <summary>
+    /// Called by an Enemy ship whenever it is destroyed. It sometimes creates
+    /// a PowerUp in place of the destroyed ship.
+    /// </summary>
+    /// <param name="e">The Enemy that was destroyed</param>
+    static public void SHIP_DESTROYED(Enemy e){
+        // Potentially generate a PowerUp
+        if (Random.value <= e.powerUpDropChance){ // Underlined red for now
+            // Choose a PowerUp from the possibilities in powerUpFrequency
+            int ndx = Random.Range(0, S.powerUpFrequency.Length);
+            eWeaponType pUpType = S.powerUpFrequency[ndx];
+
+            // Spawn a PowerUp
+            GameObject go = Instantiate<GameObject>(S.prefabPowerUp);
+            PowerUp pUp = go.GetComponent<PowerUp>();
+            // Set it to the proper WeaponType
+            pUp.SetType(pUpType);
+
+            // Set it to the position of the destroyed ship
+            pUp.transform.position = e.transform.position;
+        }
+    }
+
+
+}
